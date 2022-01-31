0520ad44850715d0ff244c6d76cbfe6bf2b3b246
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-01-31 15:01:40 +0800

    ok

--- src/js/index.js
+++ src/js/index.js
@@ -183,7 +183,7 @@ async function title_screen() {
         chalk.red(figlet.textSync('War')) + '\n' + 
         chalk.green(figlet.textSync('And')) + '\n' + 
         chalk.blue(figlet.textSync('Civilization')) + '\n\n' +
-        chalk.yellow('By: ') + chalk.cyan('AlphaBeta906') + ' - ' + chalk.red('v1.2') + '\n\n'
+        chalk.yellow('By: ') + chalk.cyan('AlphaBeta906') + ' - ' + chalk.red('v1.3') + '\n\n'
     );    
 
     inquirer.prompt([
